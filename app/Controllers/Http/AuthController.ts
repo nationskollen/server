dd3ccbe33f3fd98b5113ae3ccff523c9ea40ae32
Fredrik Engstrand <frewacom@gmail.com>
2021-03-29 16:38:37 +0200

    Added user validator

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,10 +1,10 @@
 import User from 'App/Models/User'
+import UserValidator from 'App/Validators/UserValidator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class AuthController {
     public async login({ request, auth }: HttpContextContract) {
-        const email = request.input('email')
-        const password = request.input('password')
+        const { email, password } = await request.validate(UserValidator)
 
         const token = await auth.use('api').attempt(email, password)
         return token.toJSON()
---
+++ app/Validators/UserValidator.ts
@@ -0,0 +1,18 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UserValidator {
+    constructor (protected ctx: HttpContextContract) {}
+
+    public schema = schema.create({
+        email: schema.string({}, [
+            rules.email(),
+        ]),
+        password: schema.string({}, [
+            rules.minLength(8),
+            rules.maxLength(256),
+        ]),
+    })
+
+    public messages = {}
+}
