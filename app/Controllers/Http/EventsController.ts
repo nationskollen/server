a6d6c0b39b71ea6f6b1a54c4f7167cfcf1f7d123
mcfrappe <fahadrami.jamil.9983@student.uu.se>
2021-04-13 21:37:30 +0200

    added checking for wether location that the event is added to exists and it is part of the nation where the event is being added to

--- app/Controllers/Http/EventsController.ts
+++ app/Controllers/Http/EventsController.ts
@@ -1,10 +1,12 @@
 import Event from 'App/Models/Event'
+import Location from 'App/Models/Location'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { attemptFileUpload, attemptFileRemoval } from 'App/Utils/Upload'
 import { getNation, getEvent, getValidatedData } from 'App/Utils/Request'
 import EventUpdateValidator from 'App/Validators/Events/UpdateValidator'
 import EventCreateValidator from 'App/Validators/Events/CreateValidator'
 import EventUploadValidator from 'App/Validators/Events/UploadValidator'
+import EventNotApplicableException from 'App/Exceptions/EventNotApplicableException'
 
 export default class EventsController {
     public async index({ request }: HttpContextContract) {
@@ -21,6 +23,19 @@ export default class EventsController {
     public async create({ request }: HttpContextContract) {
         const nation = getNation(request)
         const data = await getValidatedData(request, EventCreateValidator)
+
+        // Check wether the event has a location or not
+        if (data.location_id){
+
+            const location = await Location.find(data.location_id)
+
+            // Make sure the location that is the event is to be added to
+            // exists as part of the nation
+            if (!location || location.nationId !== nation.oid) {
+                throw new EventNotApplicableException();
+            }
+        }
+
         const event = await nation.related('events').create(data)
 
         return event.toJSON()
