d4cac3a5f65dc4d1df52bf3958cc84a3a98d6e13
Fredrik Engstrand <frewacom@gmail.com>
2021-04-19 15:03:30 +0200

    Use ASSET_HOSTNAME to calculcate absolute URLs (#76)

--- app/Utils/Constants.ts
+++ app/Utils/Constants.ts
@@ -1,2 +1,6 @@
-export const HOSTNAME = `http://${process.env.HOST}:${process.env.PORT}`
+/**
+ * Generates the hostname for the API.
+ * Mainly used for calculating the absolute URL for public assets.
+ */
+export const HOSTNAME = process.env.ASSET_HOSTNAME ? process.env.ASSET_HOSTNAME : `http://${process.env.HOST}:${process.env.PORT}`
 export const BASE_URL = `${HOSTNAME}/api/v1`
