fd009f759b9585f14968db9f59d104a6365a1da7
mcfrappe <fahadrami.jamil.9983@student.uu.se>
2021-04-23 10:08:44 +0200

    made category name notNullable()

--- database/migrations/1618079803910_categories.ts
+++ database/migrations/1618079803910_categories.ts
@@ -7,7 +7,7 @@ export default class Categories extends BaseSchema {
     public async up() {
         this.schema.createTable(this.tableName, (table) => {
             table.increments('id')
-            table.string('name')
+            table.string('name').notNullable()
             table.timestamps(true)
         })
     }
