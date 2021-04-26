f5762ca0cc2732aadff9a1d001e7c4c34a5fe3fe
Fredrik Engstrand <frewacom@gmail.com>
2021-04-26 15:18:16 +0200

    Added push notification ticket error handling

--- app/Services/Expo.ts
+++ app/Services/Expo.ts
@@ -8,10 +8,11 @@
  * @category Services
  * @module Expo
  */
-import { Expo, ExpoPushTicket, ExpoPushMessage } from 'expo-server-sdk'
 import Env from '@ioc:Adonis/Core/Env'
 import Logger from '@ioc:Adonis/Core/Logger'
+import PushToken from 'App/Models/PushToken'
 import Subscription from 'App/Models/Subscription'
+import { Expo, ExpoPushTicket, ExpoPushMessage } from 'expo-server-sdk'
 
 class ExpoService {
     private expo?: Expo
@@ -27,6 +28,73 @@ class ExpoService {
         }
 
         this.expo = new Expo({ accessToken })
+    }
+
+    /**
+     * Deletes the push token and all related subscriptions.
+     * Note that this assumes that the push token has been preloaded.
+     *
+     * @param subscription - The subscription that caused a `DeviceNotRegistered` error
+     */
+    private async deletePushToken(subscription?: Subscription) {
+        if (!subscription) {
+            Logger.error(
+                'Could not delete subscription for unregistered Expo push token. Subscription model is undefined'
+            )
+            return
+        }
+
+        // Save push token so taht we can delete later
+        const pushToken = subscription.pushToken
+
+        // Delete all subscriptions for this push token
+        await Subscription.query().where('pushTokenId', subscription.pushToken.id).delete()
+        await pushToken.delete()
+    }
+
+    /**
+     * Validates the response (ticket) of each sent notification.
+     * If any ticket contains an error, this must be handled accordingly.
+     * In some cases, the error will indicate that the Expo push token is invalid
+     * and we must make sure to stop sending notifications to it.
+     *
+     * @see {@link https://docs.expo.io/push-notifications/sending-notifications/#individual-errors|Expo response errors}
+     *
+     * @param tickets - The tickets to validate
+     * @param subscriptions - The subscriptions that created the tickets, in the same order
+     */
+    private validateTickets(tickets: Array<ExpoPushTicket>, subscriptions: Array<Subscription>) {
+        tickets.forEach((ticket, index) => {
+            if (ticket.status !== 'ok') {
+                switch (ticket.details?.error) {
+                    case 'MessageTooBig': /* fallthrough */
+                    case 'MessageRateExceeded':
+                        Logger.error(`Could not send push notification: ${ticket.details.error}`)
+                        break
+                    case 'InvalidCredentials':
+                        Logger.error(
+                            'Invalid Expo push credentials, Please re-generate your credentials and restart'
+                        )
+                        this.expo = undefined
+                        break
+                    case 'DeviceNotRegistered':
+                        if (index >= subscriptions.length) {
+                            Logger.error(
+                                'Could not remove unregistered Expo push token. Ticket index is out-of-bounds for subscriptions'
+                            )
+                            return
+                        }
+
+                        // The tickets are received in the same order as the notifications were sent.
+                        // I.e., the index of the current ticket is related to the subscription
+                        // with the same index. We use this to find the correct push token to remove.
+                        this.deletePushToken(subscriptions[index])
+                        break
+                }
+            } else {
+                // TODO: Save receipt ids and validate later when they have been delivered
+            }
+        })
     }
 
     /**
@@ -109,17 +177,14 @@ class ExpoService {
         for (const chunk of chunks) {
             try {
                 const ticketChunk = await this.expo.sendPushNotificationsAsync(chunk)
-
-                // TODO: Handle ticket error
-                // NOTE: If a ticket contains an error code in ticket.details.error, you
-                // must handle it appropriately. The error codes are listed in the Expo
-                // documentation:
-                // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
                 tickets.push(...ticketChunk)
             } catch (error) {
                 Logger.error(error)
             }
         }
+
+        // Validate the returned tickets and handle any errors
+        this.validateTickets(tickets, applicableSubscriptions)
     }
 }
 
