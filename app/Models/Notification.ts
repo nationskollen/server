2ecf15fc6cd1246494a25108246fc6f30d85e240
mcfrappe <fahadrami.jamil.9983@student.uu.se>
2021-04-29 14:47:45 +0200

    added serialization for the updated_at and created_at field

--- app/Models/Notification.ts
+++ app/Models/Notification.ts
@@ -11,9 +11,9 @@ export default class Notification extends BaseModel {
     @column()
     public message: string
 
-    @column.dateTime({ autoCreate: true })
+    @column.dateTime({ autoCreate: true, serializeAs: null })
     public createdAt: DateTime
 
-    @column.dateTime({ autoCreate: true, autoUpdate: true })
+    @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
     public updatedAt: DateTime
 }
